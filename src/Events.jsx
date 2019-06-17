3fe00ca38fa69ae97bc06abb0a0aec80b230919a
caterina <ianesellicaterina@gmail.com>
2019-06-17 15:19:26 +0200

    fix grafici finali

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+// import Header from "./../components/Header";
+// import "./Eventi.scss";
+
+// import Loader from "./../components/Loader";
+import img_sostitutiva from "./icone/mohole.jpg";
 
 class Card extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
-
   render() {
     return (
       <div className="card" key={this.props.k}>
         <div class="card-count-container">
           <img src={this.props.img} alt={this.props.altImg} />
-          <div class="card-count">{this.props.k}</div>
+          <div class="card-count"
+            dangerouslySetInnerHTML={{
+              __html: this.props.calendar
+            }}
+          />
+          {/* <div class="card-count">{this.props.calendar}</div> */}
         </div>
         <div className="card-content">
           <h3>{this.props.title}</h3>
@@ -20,8 +29,7 @@ class Card extends React.Component {
               __html: this.props.excerpt
             }}
           />
-
-        </div> 
+        </div>
       </div>
     );
   }
@@ -44,11 +52,16 @@ class Eventi extends React.Component {
       ),
       fetch("https://moholepeople.it/dashboard/wp-json/wp/v2/media/").then(
         media => media.json()
+      ),
+      fetch("https://moholepeople.it/dashboard/wp-json/acf/v3/posts").then(
+        data => data.json()
       )
-    ]).then(([posts, media]) => {
+    ]).then(([posts, media, calendar, categories]) => {
       this.setState({
         posts,
         media,
+        calendar,
+        categories,
         loading: false
       });
       console.log(this.state);
@@ -59,26 +72,60 @@ class Eventi extends React.Component {
     console.log(this.state);
   }
 
+  scrollCards(e) {
+    // console.log(e.target)
+    let cards = e.target.childNodes[2].childNodes;
+    // console.log(cards)
+    // let card = e.target.firstChild;
+    // let cardHeight = e.target.firstChild.clientHeight;
+    let cardHeight = cards.clientHeight;
+    cards.forEach(c => {
+      // console.log(c)
+      // console.log('c offset letf '  +  c.offsetTop)
+      let offsetBottom = c.offsetTop + cardHeight;
+      // console.log('c offset right '+ offsetRight);
+      let topContainer = e.target.scrollTop;
+      let bottomContainer = e.target.scrollTop + window.screen.width;
+      // console.log('cont offset left '+ leftContainer);
+      // console.log('cont offset right '+ bottomContainer);
+      if (topContainer < c.offsetTop && offsetBottom < bottomContainer) {
+        console.log(c);
+        // c.classList.add("focus");
+      } else {
+        // c.classList.remove("focus");
+      }
+    });
+  }
+
   render() {
     const cards = this.state.posts.map((p, i) => {
       const featured = this.state.media.find(
         media => p.featured_media === media.id
+      );
+      const calendar = this.state.calendar.find(
+        calendar => p.id === calendar.id
       );
 
       return (
         <Card
           key={i}
           title={p.title.rendered}
-          img={featured ? featured.source_url : <p> iim</p>}
+          img={featured ? featured.source_url : img_sostitutiva}
           altImg="alt"
           excerpt={p.excerpt.rendered}
+          calendar={
+            calendar
+              ? calendar.acf.date_and_time
+              : "DATA EVENTO NON SPECIFICATA"
+          }
         />
       );
     });
     return (
-      <div className="content">
-        {this.state.loading ? <p>Caricamento</p> : ""}
-        <div class="card-container">{cards}</div>
+      <div className="content" onScroll={this.scrollCards.bind(this)}>
+        {/* {this.state.loading ? <Loader /> : ""} */}
+        {/* <Header  titoloPagina='Eventi'/> */}
+        <div className="card-container">{cards}</div>
       </div>
     );
   }
